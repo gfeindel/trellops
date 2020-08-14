0f7a7045d424ec77644c8ff0f3a11be1623989d7
Gabe Feindel <gabefeindel@gmail.com>
2020-08-14 16:15:07 -0400

    Added label names to exported data; Priority based on label.

--- Export-TrelloBoard.ps1
+++ Export-TrelloBoard.ps1
@@ -23,14 +23,21 @@ $lists |% {
             Sprint = $d.Month
             List = $_.Name
             Effort = 0
-            Priority = 0
+            Priority = ""
+            Labels = @()
+        }
+        if($card.labels) {
+            $o.Labels = [string]::Join(',',$card.labels.name)
+            foreach($label in $card.labels) {
+                if($label.name -in ('A','B','C','D')) { $o.Priority = $label.name}
+            }
         }
         if($card.name -match "\(\d+\)") {
             $o.Effort = [int](($card.Name | Select-String -Pattern "\((\d+)\)").Matches[0].Groups[1].Value)
         }
-        if($card.name -match "#\d+") {
-            $o.Priority = [int](($card.Name | Select-String -Pattern "#(\d+)").Matches[0].Groups[1].Value)
-        }
+        #if($card.labels -match "#\d+") {
+        #    $o.Priority = [int](($card.Name | Select-String -Pattern "#(\d+)").Matches[0].Groups[1].Value)
+        #}
         $o
     }
 }
